f9e95d606868af666ead30c2fbb68387201f2111
Peter Rose <peterrose1994@gmail.com>
2021-12-16 20:19:35 -0500

    Made common library into a springboot library

--- src/client/gui/src/main/java/com/kdyncs/dragonsong/client/gui/components/ChatView.java
+++ src/client/gui/src/main/java/com/kdyncs/dragonsong/client/gui/components/ChatView.java
@@ -1,22 +1,26 @@
 package com.kdyncs.dragonsong.client.gui.components;
 
 import com.kdyncs.dragonsong.client.common.DragonConfig;
+import com.kdyncs.dragonsong.client.common.DragonSong;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 @FxmlView("/view/ChatView.fxml")
 public class ChatView {
 
-    private final DragonConfig config;
+    private final DragonSong server;
     private final FxWeaver weaver;
 
     @Autowired
-    public ChatView(DragonConfig config, FxWeaver weaver) {
-        this.config = config;
+    public ChatView(DragonSong server, FxWeaver weaver) {
+        this.server = server;
         this.weaver = weaver;
-    }
 
+        server.connect();
+    }
 }
