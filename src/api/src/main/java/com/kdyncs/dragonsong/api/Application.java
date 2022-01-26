aecd2d90aac9b96e5d29a6457786c5ce6cfefe71
Peter Rose <peterrose1994@gmail.com>
2022-01-25 19:06:06 -0500

    Fixed issues that prevented the API from booting

--- src/api/src/main/java/com/kdyncs/dragonsong/api/Application.java
+++ src/api/src/main/java/com/kdyncs/dragonsong/api/Application.java
@@ -23,6 +23,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -30,6 +31,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.kdyncs.dragonsong")
 @EnableJpaRepositories("com.kdyncs.dragonsong")
+@EntityScan("com.kdyncs.dragonsong")
 @EnableScheduling
 public class Application {
     
